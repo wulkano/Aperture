bda22a02af3e6e35f41055f0917b59d3d4129b5c
Matheus Fernandes <github@matheus.top>
2016-08-11 19:28:56 -0300

    Check process type – closes #1

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const {desktopCapturer} = require('electron');
 
+function checkProcessType(expected, msgPrefix) {
+	if (expected === 'main' && process.type !== 'browser') {
+		throw new Error(`${msgPrefix} should be runned in Electron's main process`);
+	}
+	if (expected === 'renderer' && process.type !== expected) {
+		throw new Error(`${msgPrefix} should be runned in Electron's renderer process`);
+	}
+
+	if (['main', 'renderer'].indexOf(expected) === -1) {
+		throw new Error(`${expected} is not a known Electron process type`);
+	}
+}
+
 function initMain() {
-	console.log('init main');
+	checkProcessType('main', 'aperture.main');
+	// TODO
 }
 
 function initRenderer() {
+	checkProcessType('renderer', 'aperture.renderer');
+
 	desktopCapturer.getSources({types: ['screen']}, (err, sources) => {
 		if (err) {
 			throw err;
