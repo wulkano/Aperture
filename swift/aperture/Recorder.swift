77951c0347b93889669700acce1f4171c0f1dbd1
Matheus Fernandes <github@matheus.top>
2016-09-02 03:06:13 -0300

    Introduce a Xcode project to build correctly

---
+++ swift/aperture/Recorder.swift
@@ -0,0 +1,63 @@
+import AVFoundation;
+
+public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
+    var destination: NSURL?;
+    var session: AVCaptureSession?;
+    var input: AVCaptureScreenInput?
+    var output: AVCaptureMovieFileOutput?;
+    
+    public init(fps: String) {
+        super.init();
+        self.session = AVCaptureSession();
+        
+        let displayId: CGDirectDisplayID = CGMainDisplayID();
+        
+        self.input = AVCaptureScreenInput(displayID: displayId);
+        
+        if ((self.session?.canAddInput(input)) != nil) {
+            self.session?.addInput(input);
+        } else {
+            print("can't add input"); // TODO
+        }
+        
+        self.output = AVCaptureMovieFileOutput();
+        self.output?.movieFragmentInterval = CMTimeMake(1,1); // write data to file every 1 second
+        
+        if ((self.session?.canAddOutput(self.output)) != nil) {
+            self.session?.addOutput(self.output);
+        } else {
+            print("can't add output"); // TODO
+        }
+        
+        let conn = self.output?.connectionWithMediaType(AVMediaTypeVideo);
+        let cmTime = CMTimeMake(1, Int32(fps)!);
+        conn?.videoMinFrameDuration = cmTime; // TODO check if can set
+        conn?.videoMaxFrameDuration = cmTime; // TODO ^^^^^^^^^^^^^^^^
+    }
+    
+    public func start(destinationPath: String, coordinates: [String]) {
+        self.destination = NSURL.fileURLWithPath(destinationPath);
+        
+        if (coordinates.count != 0) {
+            let points = coordinates.map { CGFloat((Int($0))!) };
+            let rect = CGRectMake(points[0], points[1], points[2], points[3]); // x, y, width, height
+            self.input?.cropRect = rect;
+        }
+        
+        self.session?.startRunning();
+        self.output?.startRecordingToOutputFileURL(self.destination, recordingDelegate: self);
+    }
+    
+    public func stop() {
+        self.output?.stopRecording();
+        self.session?.stopRunning();
+    }
+    
+    public func captureOutput(captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAtURL fileURL: NSURL!, fromConnections connections: [AnyObject]!) {
+        print("R"); // at this point the recording really started
+    }
+    
+    public func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+        print(error);
+    }
+}
--- swift/aperture/main.swift
+++ swift/aperture/main.swift
@@ -8,10 +8,10 @@ import AVFoundation
 
 let numberOfArgs = Process.arguments.count;
 if (numberOfArgs != 3 && numberOfArgs != 4) {
-	print("usage: main destinationPath fps [crop rect coordinates]")
-	print("examples: main ./file.mp4 30");
-	print("          main ./file.mp4 30 0:0:100:100");
-	exit(1);
+    print("usage: main destinationPath fps [crop rect coordinates]")
+    print("examples: main ./file.mp4 30");
+    print("          main ./file.mp4 30 0:0:100:100");
+    exit(1);
 }
 
 let destinationPath = Process.arguments[1];
@@ -19,11 +19,11 @@ let fps = Process.arguments[2];
 
 var coordinates = [];
 if (numberOfArgs == 4) {
-	coordinates = Process.arguments[3].componentsSeparatedByString(":");
-	if (coordinates.count - 1 != 3) { // number of ':' in the string
-		print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'");
-		exit(2);
-	}
+    coordinates = Process.arguments[3].componentsSeparatedByString(":");
+    if (coordinates.count - 1 != 3) { // number of ':' in the string
+        print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'");
+        exit(2);
+    }
 }
 
 let recorder = Recorder(fps: fps);
