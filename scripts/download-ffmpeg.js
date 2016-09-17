8953a57b28e2a1aee268e2cc0abac619e7232228
Matheus Fernandes <github@matheus.top>
2016-09-17 17:22:43 -0300

    Fix code style

--- scripts/download-ffmpeg.js
+++ scripts/download-ffmpeg.js
@@ -12,27 +12,26 @@ let spinner = ora({text: 'Downloading 7zip', stream: process.stdout}).start();
 const FFMPEG_URL = 'http://evermeet.cx/ffmpeg/ffmpeg-81632-g09317e3.7z';
 
 const joinPath = (...str) => path.join(__dirname, ...str);
-const which = cmd => execa.sync(joinPath('which.sh'), [cmd]).stdout
+const which = cmd => execa.sync(joinPath('which.sh'), [cmd]).stdout;
 const cmdExists = cmd => which(cmd) !== '';
 const logErrorAndExit = msg => {
-	spinner.fail();
-	console.error(chalk.red(msg));
-	process.exit(1);
+  spinner.fail();
+  console.error(chalk.red(msg));
+  process.exit(1);
 };
 
 if (process.platform === 'darwin') { // macOS
-	if (!cmdExists('brew')) {
-		let msg = `${chalk.bold('aperture')} needs ${chalk.bold('brew')} in order to `;
-		msg += `automagically download ${chalk.bold('ffmpeg')}.`;
-		// TODO add a link to a README.md section that explains what's going on here
-		logErrorAndExit(msg);
-	}
+  if (!cmdExists('brew')) {
+    let msg = `${chalk.bold('aperture')} needs ${chalk.bold('brew')} in order to `;
+    msg += `automagically download ${chalk.bold('ffmpeg')}.`;
+    // TODO add a link to a README.md section that explains what's going on here
+    logErrorAndExit(msg);
+  }
 
-	execa(joinPath('brew-install-7zip.sh'))
-		.then(() => {
+  execa(joinPath('brew-install-7zip.sh'))
+    .then(() => {
       spinner.succeed();
       spinner = ora({text: 'Downloading ffmpeg', stream: process.stdout}).start();
-      const ffmpegPath = which('ffmpeg');
       fs.mkdir(joinPath('..', 'vendor'), err => {
         if (err) {
           if (err.code !== 'EEXIST') {
@@ -54,7 +53,7 @@ if (process.platform === 'darwin') { // macOS
         }
       });
     })
-		.catch(logErrorAndExit);
+    .catch(logErrorAndExit);
 } else {
   logErrorAndExit(`${chalk.bold('aperture.js')} only support macOS for now`);
 }
