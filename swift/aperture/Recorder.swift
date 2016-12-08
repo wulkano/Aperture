wulkano/Aperture

commit 17186eb9551c54620a6a8a3c79bc6a9f53e34889

Fix all the compression issues (#10)
* Fix all the compression issues

Fixes trimming in QuickTime, VLC hanging, Twitter/Instagram rejecting the video.

Fixes wulkano/kap#22
Fixes wulkano/kap#69
Fixes wulkano/kap#85

* Gracefully stop the recording

* Wait for the recording to finish when stopping it

It's async

* Ignore "Stop Recording" error

swift/aperture/Recorder.swift
@@ -20,7 +20,6 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
     }
 
     self.output = AVCaptureMovieFileOutput();
-    self.output?.movieFragmentInterval = CMTimeMake(1,1); // write data to file every 1 second
 
     if ((self.session?.canAddOutput(self.output)) != nil) {
       self.session?.addOutput(self.output);
@@ -55,6 +54,16 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
   }
 
   public func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
-    print(error);
+    if error != nil {
+      // don't print useless "Stop Recording" error
+      if (error.code != -11806) {
+        print(error);
+      }
+
+      // TODO: Make `stop()` accept a callback that is called when this method is called and do the exiting in `main.swift`
+      exit(1);
+    } else {
+      exit(0);
+    }
   }
 }

swift/aperture/main.swift
@@ -35,10 +35,15 @@ if (cropArea != "none") {
 
 let recorder = Recorder(destinationPath: destinationPath, fps: fps, coordinates: coordinates as! [String], showCursor: showCursor, highlightClicks: highlightClicks, displayId: displayId!);
 
+func quit(_: Int32) {
+	recorder.stop();
+}
+
+signal(SIGHUP, quit);
+signal(SIGINT, quit);
+signal(SIGTERM, quit);
+signal(SIGQUIT, quit);
+
 recorder.start();
 
-setbuf(__stdoutp, nil);
-
-readLine();
-
-recorder.stop();
+NSRunLoop.mainRunLoop().run();

index.js
@@ -90,7 +90,7 @@ class Aperture {
         }
       });
 
-      this.recorder.stdin.write('\n');
+      this.recorder.kill();
     });
   }
 }

commit e6666ecd61b2d6b12e4d9e8083fcafb20c8dbf0e

Fix breaking changes introduced by #10

swift/aperture/Recorder.swift
@@ -69,16 +69,17 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
   }
 
   public func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+    // TODO: Make `stop()` accept a callback that is called when this method is called and do the exiting in `main.swift`
     if error != nil {
       // don't print useless "Stop Recording" error
       if (error.code != -11806) {
         print(error);
+        exit(1);
+      } else {
+        exit(0);
       }
-
-      // TODO: Make `stop()` accept a callback that is called when this method is called and do the exiting in `main.swift`
-      exit(1);
     } else {
-      exit(0);
+      exit(0); // TODO: this will probably never happen, check if we can remove the if-else
     }
   }
 }

swift/aperture/main.swift
@@ -49,6 +49,7 @@ func record() {
   signal(SIGQUIT, quit);
 
   recorder?.start();
+  setbuf(__stdoutp, nil);
 
   NSRunLoop.mainRunLoop().run();
 }