32848e72b354a05b777e6777ea7240deef4d98f7
Khaled Garbaya <khaledgarbaya@gmail.com>
2016-12-08 19:40:11 +0000

    [WIP] Add Ability to Record Audio (#6)
    * [WIP] Add Ability to Record Audio
    
    * Add shared Scheme
    
    * WIP Add Listing of Audio input devices, swift side
    
    * Add getAudioSources to Aperture
    
    * Code review Changes
    
    * fixup! Code review Changes
    
    * fixup! fixup! Code review Changes
    
    * fixup! fixup! fixup! Code review Changes
    
    * Fix code style
    
    * Fix intermittent error when recording using specified mic
    
    No more "FigMovieFormatFileWriter::PostProcessMovie: WriteMovie() errored!!! -12780"!
    
    * Add the ability to record a specific audio device and fix bad merge

--- aperture/Recorder.swift
+++ aperture/Recorder.swift
@@ -4,19 +4,34 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
   var destination: NSURL?;
   var session: AVCaptureSession?;
   var input: AVCaptureScreenInput?
+  var audioInput: AVCaptureDeviceInput?
   var output: AVCaptureMovieFileOutput?;
 
-  public init(destinationPath: String, fps: String, coordinates: [String], showCursor: Bool, highlightClicks: Bool, displayId: UInt32) {
+  public init(destinationPath: String, fps: String, coordinates: [String], showCursor: Bool, highlightClicks: Bool, displayId: UInt32, audioDeviceId: String) {
     super.init();
     self.session = AVCaptureSession();
 
     self.input = AVCaptureScreenInput(displayID: displayId);
     self.input!.minFrameDuration = CMTimeMake(1, Int32(fps)!);
 
+    if (audioDeviceId != "none") {
+      let audioDevice: AVCaptureDevice = AVCaptureDevice.init(uniqueID: audioDeviceId);
+
+      do {
+        try self.audioInput = AVCaptureDeviceInput(device: audioDevice);
+
+        if ((self.session?.canAddInput(self.audioInput)) != nil) {
+          self.session?.addInput(self.audioInput);
+        } else {
+          print("Can't add audio input");
+        }
+      } catch {} // TODO
+    }
+
     if ((self.session?.canAddInput(input)) != nil) {
       self.session?.addInput(input);
     } else {
-      print("can't add input"); // TODO
+      print("Can't add input"); // TODO
     }
 
     self.output = AVCaptureMovieFileOutput();
--- aperture/main.swift
+++ aperture/main.swift
@@ -9,41 +9,72 @@
 import Foundation
 import AVFoundation
 
-let numberOfArgs = Process.arguments.count;
-if (numberOfArgs != 7) {
-  print("usage: main <destinationPath> <fps> <crop-rect-coordinates> <show-cursor> <highlight-clicks> <display-id>")
-  print("examples: main ./file.mp4 30 0:0:100:100 true false 1846519");
-  print("examples: main ./file.mp4 30 none true false main");
-  exit(1);
-}
-
-let destinationPath = Process.arguments[1];
-let fps = Process.arguments[2];
-let cropArea = Process.arguments[3];
-let showCursor = Process.arguments[4] == "true" ? true : false;
-let highlightClicks = Process.arguments[5] == "true" ? true : false;
-let displayId = Process.arguments[6] == "main" ? CGMainDisplayID() : UInt32(Process.arguments[6]);
-
-var coordinates = [];
-if (cropArea != "none") {
-  coordinates = Process.arguments[3].componentsSeparatedByString(":");
-  if (coordinates.count - 1 != 3) { // number of ':' in the string
-    print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'");
-    exit(2);
-  }
-}
-
-let recorder = Recorder(destinationPath: destinationPath, fps: fps, coordinates: coordinates as! [String], showCursor: showCursor, highlightClicks: highlightClicks, displayId: displayId!);
+var recorder: Recorder?;
 
 func quit(_: Int32) {
-	recorder.stop();
+  recorder?.stop();
 }
 
-signal(SIGHUP, quit);
-signal(SIGINT, quit);
-signal(SIGTERM, quit);
-signal(SIGQUIT, quit);
+func record() {
+  let destinationPath = Process.arguments[1];
+  let fps = Process.arguments[2];
+  let cropArea = Process.arguments[3];
+  let showCursor = Process.arguments[4] == "true" ? true : false;
+  let highlightClicks = Process.arguments[5] == "true" ? true : false;
+  let displayId = Process.arguments[6] == "main" ? CGMainDisplayID() : UInt32(Process.arguments[6]);
+  let audioDeviceId = Process.arguments[7];
 
-recorder.start();
+  var coordinates = [];
+  if (cropArea != "none") {
+    coordinates = Process.arguments[3].componentsSeparatedByString(":");
+    if (coordinates.count - 1 != 3) { // number of ':' in the string
+      print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'");
+      exit(2);
+    }
+  }
 
-NSRunLoop.mainRunLoop().run();
+  recorder = Recorder(
+    destinationPath: destinationPath,
+    fps: fps,
+    coordinates: coordinates as! [String],
+    showCursor: showCursor,
+    highlightClicks: highlightClicks,
+    displayId: displayId!,
+    audioDeviceId: audioDeviceId
+  );
+
+  signal(SIGHUP, quit);
+  signal(SIGINT, quit);
+  signal(SIGTERM, quit);
+  signal(SIGQUIT, quit);
+
+  recorder?.start();
+
+  NSRunLoop.mainRunLoop().run();
+}
+
+func listAudioDevices() {
+  print(AudioDeviceList().getInputDevices() as! String);
+}
+
+func usage() {
+  print("usage: main <list-audio-devices | <destinationPath> <fps> <crop-rect-coordinates> <show-cursor> <highlight-clicks> <display-id> <audio-device-id>>");
+  print("examples: main ./file.mp4 30 0:0:100:100 true false 1846519 'AppleHDAEngineInput:1B,0,1,0:1'");
+  print("          main ./file.mp4 30 none true false main none");
+  print("          main list-audio-devices");
+}
+
+let numberOfArgs = Process.arguments.count
+
+if (numberOfArgs == 8) {
+  record();
+  exit(0);
+}
+
+if (numberOfArgs == 2 && Process.arguments[1] == "list-audio-devices"){
+  listAudioDevices();
+  exit(0);
+}
+
+usage();
+exit(1);
