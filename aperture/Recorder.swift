cad26c9ea5157c4409b18ec38ebbab47c26fa0fd
Sindre Sorhus <sindresorhus@gmail.com>
2016-12-12 15:58:42 +0100

    Upgrade to Swift 3 (#12)
    The automatic migrator managed to do most of the work, but I did have to fix a couple of compile errors and do some minor tweaks.

--- aperture/Recorder.swift
+++ aperture/Recorder.swift
@@ -1,13 +1,13 @@
 import AVFoundation;
 
-public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
-  var destination: NSURL?;
+class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
+  var destination: URL?;
   var session: AVCaptureSession?;
   var input: AVCaptureScreenInput?
   var audioInput: AVCaptureDeviceInput?
   var output: AVCaptureMovieFileOutput?;
 
-  public init(destinationPath: String, fps: String, coordinates: [String], showCursor: Bool, highlightClicks: Bool, displayId: UInt32, audioDeviceId: String) {
+  init(destinationPath: String, fps: String, coordinates: [String], showCursor: Bool, highlightClicks: Bool, displayId: UInt32, audioDeviceId: String) {
     super.init();
     self.session = AVCaptureSession();
 
@@ -42,11 +42,11 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
       print("can't add output"); // TODO
     }
 
-    self.destination = NSURL.fileURLWithPath(destinationPath);
+    self.destination = URL(fileURLWithPath: destinationPath);
 
     if (coordinates.count != 0) {
       let points = coordinates.map { CGFloat((Int($0))!) };
-      let rect = CGRectMake(points[0], points[1], points[2], points[3]); // x, y, width, height
+      let rect = CGRect(x: points[0], y: points[1], width: points[2], height: points[3]); // x, y, width, height
       self.input?.cropRect = rect;
     }
 
@@ -54,25 +54,25 @@ public class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
     self.input?.capturesMouseClicks = highlightClicks;
   }
 
-  public func start() {
+  func start() {
       self.session?.startRunning();
-      self.output?.startRecordingToOutputFileURL(self.destination, recordingDelegate: self);
+      self.output?.startRecording(toOutputFileURL: self.destination, recordingDelegate: self);
     }
 
-  public func stop() {
+  func stop() {
       self.output?.stopRecording();
       self.session?.stopRunning();
   }
 
-  public func captureOutput(captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAtURL fileURL: NSURL!, fromConnections connections: [AnyObject]!) {
+  func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
     print("R"); // at this point the recording really started
   }
 
-  public func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+  func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
     // TODO: Make `stop()` accept a callback that is called when this method is called and do the exiting in `main.swift`
     if error != nil {
       // don't print useless "Stop Recording" error
-      if (error.code != -11806) {
+      if (error._code != -11806) {
         print(error);
         exit(1);
       } else {
