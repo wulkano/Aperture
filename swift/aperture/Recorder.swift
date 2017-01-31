156e1886d115952e75e7c57d8418caf56a8786db
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-31 11:20:01 +0700

    Swift code style

--- swift/aperture/Recorder.swift
+++ swift/aperture/Recorder.swift
@@ -1,101 +1,101 @@
-import AVFoundation;
+import AVFoundation
 
 class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate {
-  var destination: URL?;
-  var session: AVCaptureSession?;
+  var destination: URL?
+  var session: AVCaptureSession?
   var input: AVCaptureScreenInput?
   var audioInput: AVCaptureDeviceInput?
-  var output: AVCaptureMovieFileOutput?;
+  var output: AVCaptureMovieFileOutput?
 
   init(destinationPath: String, fps: String, coordinates: [String], showCursor: Bool, highlightClicks: Bool, displayId: UInt32, audioDeviceId: String) {
-    super.init();
-    self.session = AVCaptureSession();
+    super.init()
+    self.session = AVCaptureSession()
 
-    self.input = AVCaptureScreenInput(displayID: displayId);
-    self.input!.minFrameDuration = CMTimeMake(1, Int32(fps)!);
+    self.input = AVCaptureScreenInput(displayID: displayId)
+    self.input!.minFrameDuration = CMTimeMake(1, Int32(fps)!)
 
-    if (audioDeviceId != "none") {
-      let audioDevice: AVCaptureDevice = AVCaptureDevice.init(uniqueID: audioDeviceId);
+    if audioDeviceId != "none" {
+      let audioDevice: AVCaptureDevice = AVCaptureDevice.init(uniqueID: audioDeviceId)
 
       do {
-        try self.audioInput = AVCaptureDeviceInput(device: audioDevice);
+        try self.audioInput = AVCaptureDeviceInput(device: audioDevice)
 
-        if ((self.session?.canAddInput(self.audioInput)) != nil) {
-          self.session?.addInput(self.audioInput);
+        if self.session?.canAddInput(self.audioInput) != nil {
+          self.session?.addInput(self.audioInput)
         } else {
           // TODO(matheuss): When we can't add the input, we should imediately exit
           // With that, the JS part would be able to `reject` the `Promise`.
           // Right now, on Kap for example, the recording will probably continue without
           // letting the user now that no audio is being recorded
-          print("Can't add audio input");
+          print("Can't add audio input")
         }
       } catch {} // TODO(matheuss): Exit when this happens
     }
 
-    if ((self.session?.canAddInput(input)) != nil) {
-      self.session?.addInput(input);
+    if self.session?.canAddInput(input) != nil {
+      self.session?.addInput(input)
     } else {
-      print("Can't add input");
+      print("Can't add input")
       // TODO(matheuss): When we can't add the input, we should imediately exit
       // With that, the JS part would be able to `reject` the `Promise`.
       // Right now, on Kap for example, the recording will probably continue without
       // letting the user now that no video is being recorded
     }
 
-    self.output = AVCaptureMovieFileOutput();
+    self.output = AVCaptureMovieFileOutput()
 
     // Needed because otherwise there is no audio on videos longer than 10 seconds
     // http://stackoverflow.com/a/26769529/64949
     self.output?.movieFragmentInterval = kCMTimeInvalid
 
-    if ((self.session?.canAddOutput(self.output)) != nil) {
-      self.session?.addOutput(self.output);
+    if self.session?.canAddOutput(self.output) != nil {
+      self.session?.addOutput(self.output)
     } else {
-      print("Can't add output");
+      print("Can't add output")
       // TODO(matheuss): When we can't add the input, we should imediately exit
       // With that, the JS part would be able to `reject` the `Promise`.
       // Right now, on Kap for example, the recording will probably continue without
       // letting the user now that no the file will not be saved
     }
 
-    self.destination = URL(fileURLWithPath: destinationPath);
+    self.destination = URL(fileURLWithPath: destinationPath)
 
-    if (coordinates.count != 0) {
-      let points = coordinates.map { CGFloat((Int($0))!) };
-      let rect = CGRect(x: points[0], y: points[1], width: points[2], height: points[3]); // x, y, width, height
-      self.input?.cropRect = rect;
+    if coordinates.count != 0 {
+      let points = coordinates.map { CGFloat((Int($0))!) }
+      let rect = CGRect(x: points[0], y: points[1], width: points[2], height: points[3]) // x, y, width, height
+      self.input?.cropRect = rect
     }
 
-    self.input?.capturesCursor = showCursor;
-    self.input?.capturesMouseClicks = highlightClicks;
+    self.input?.capturesCursor = showCursor
+    self.input?.capturesMouseClicks = highlightClicks
   }
 
   func start() {
-    self.session?.startRunning();
-    self.output?.startRecording(toOutputFileURL: self.destination, recordingDelegate: self);
+    self.session?.startRunning()
+    self.output?.startRecording(toOutputFileURL: self.destination, recordingDelegate: self)
   }
 
   func stop() {
-    self.output?.stopRecording();
-    self.session?.stopRunning();
+    self.output?.stopRecording()
+    self.session?.stopRunning()
   }
 
   func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
-    print("R"); // At this point the recording really started
+    print("R") // At this point the recording really started
   }
 
   func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
     // TODO: Make `stop()` accept a callback that is called when this method is called and do the exiting in `main.swift`
     if error != nil {
       // Don't print useless "Stop Recording" error
-      if (error._code != -11806) {
-        print(error);
-        exit(1);
+      if error._code != -11806 {
+        print(error)
+        exit(1)
       } else {
-        exit(0);
+        exit(0)
       }
     } else {
-      exit(0); // TODO(matheuss): This will probably never happen, check if we can remove the if-else
+      exit(0) // TODO(matheuss): This will probably never happen, check if we can remove the if-else
     }
   }
 }
--- swift/aperture/main.swift
+++ swift/aperture/main.swift
@@ -8,27 +8,29 @@
 import Foundation
 import AVFoundation
 
-var recorder: Recorder?;
+var recorder: Recorder?
 
 func quit(_: Int32) {
-  recorder?.stop();
+  recorder?.stop()
 }
 
 func record() {
-  let destinationPath = CommandLine.arguments[1];
-  let fps = CommandLine.arguments[2];
-  let cropArea = CommandLine.arguments[3];
-  let showCursor = CommandLine.arguments[4] == "true" ? true : false;
-  let highlightClicks = CommandLine.arguments[5] == "true" ? true : false;
-  let displayId = CommandLine.arguments[6] == "main" ? CGMainDisplayID() : UInt32(CommandLine.arguments[6]);
-  let audioDeviceId = CommandLine.arguments[7];
+  let destinationPath = CommandLine.arguments[1]
+  let fps = CommandLine.arguments[2]
+  let cropArea = CommandLine.arguments[3]
+  let showCursor = CommandLine.arguments[4] == "true"
+  let highlightClicks = CommandLine.arguments[5] == "true"
+  let displayId = CommandLine.arguments[6] == "main" ? CGMainDisplayID() : UInt32(CommandLine.arguments[6])
+  let audioDeviceId = CommandLine.arguments[7]
 
-  var coordinates = [String]();
-  if (cropArea != "none") {
-    coordinates = CommandLine.arguments[3].components(separatedBy: ":");
-    if (coordinates.count - 1 != 3) { // number of ':' in the string
-      print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'");
-      exit(2);
+  print(CommandLine.arguments[4] == "true")
+
+  var coordinates = [String]()
+  if cropArea != "none" {
+    coordinates = CommandLine.arguments[3].components(separatedBy: ":")
+    if coordinates.count - 1 != 3 { // Number of ':' in the string
+      print("The coordinates for the crop rect must be in the format 'originX:originY:width:height'")
+      exit(2)
     }
   }
 
@@ -40,41 +42,41 @@ func record() {
     highlightClicks: highlightClicks,
     displayId: displayId!,
     audioDeviceId: audioDeviceId
-  );
+  )
 
-  signal(SIGHUP, quit);
-  signal(SIGINT, quit);
-  signal(SIGTERM, quit);
-  signal(SIGQUIT, quit);
+  signal(SIGHUP, quit)
+  signal(SIGINT, quit)
+  signal(SIGTERM, quit)
+  signal(SIGQUIT, quit)
 
-  recorder?.start();
-  setbuf(__stdoutp, nil);
+  recorder?.start()
+  setbuf(__stdoutp, nil)
 
-  RunLoop.main.run();
+  RunLoop.main.run()
 }
 
 func listAudioDevices() {
-  print(AudioDeviceList().getInputDevices() as! String);
+  print(AudioDeviceList().getInputDevices() as! String)
 }
 
 func usage() {
-  print("usage: main <list-audio-devices | <destinationPath> <fps> <crop-rect-coordinates> <show-cursor> <highlight-clicks> <display-id> <audio-device-id>>");
-  print("examples: main ./file.mp4 30 0:0:100:100 true false 1846519 'AppleHDAEngineInput:1B,0,1,0:1'");
-  print("          main ./file.mp4 30 none true false main none");
-  print("          main list-audio-devices");
+  print("usage: main <list-audio-devices | <destinationPath> <fps> <crop-rect-coordinates> <show-cursor> <highlight-clicks> <display-id> <audio-device-id>>")
+  print("examples: main ./file.mp4 30 0:0:100:100 true false 1846519 'AppleHDAEngineInput:1B,0,1,0:1'")
+  print("          main ./file.mp4 30 none true false main none")
+  print("          main list-audio-devices")
 }
 
 let numberOfArgs = CommandLine.arguments.count
 
-if (numberOfArgs == 8) {
-  record();
-  exit(0);
+if numberOfArgs == 8 {
+  record()
+  exit(0)
 }
 
-if (numberOfArgs == 2 && CommandLine.arguments[1] == "list-audio-devices"){
-  listAudioDevices();
-  exit(0);
+if numberOfArgs == 2 && CommandLine.arguments[1] == "list-audio-devices" {
+  listAudioDevices()
+  exit(0)
 }
 
-usage();
-exit(1);
+usage()
+exit(1)
