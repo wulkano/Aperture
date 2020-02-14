54650a5385bce62eceb29cc003e5636f5150a32c
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-14 18:13:08 +0700

    Update Package.swift

--- ScreenRecordingSample/Package.swift
+++ ScreenRecordingSample/Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,18 +7,20 @@ let package = Package(
         .macOS(.v10_12)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "../")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ScreenRecordingSample",
-            dependencies: ["Aperture"]),
+            dependencies: [
+              "Aperture"
+            ]
+        ),
         .testTarget(
             name: "ScreenRecordingSampleTests",
-            dependencies: ["ScreenRecordingSample"]),
+            dependencies: [
+              "ScreenRecordingSample"
+            ]
+        )
     ]
 )
